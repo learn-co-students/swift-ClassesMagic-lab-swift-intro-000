7764b3f03548fc22786453d7732c88fdd0e8690a
briecoyle <brie.coyle@gmail.com>
2016-10-16 18:10:11 -0400

    Error with tests

--- ClassesMagicFun/Card.swift
+++ ClassesMagicFun/Card.swift
@@ -9,4 +9,33 @@
 import Foundation
 
 class Card {
+    let suit: String
+    let rank: String
+    var label: String {
+        return "\(rank)\(suit)"
+    }
+    var value: Int {
+        var value = 0
+        switch rank {
+        case "A": value = 1
+        case "2": value = 2
+        case "3": value = 3
+        case "4": value = 4
+        case "5": value = 5
+        case "6": value = 6
+        case "7": value = 7
+        case "8": value = 8
+        case "9": value = 9
+        case "10": value = 10
+        case "J": value = 10
+        case "Q": value = 10
+        case "K": value = 10
+        default: value = 0
+        }
+        return value
+    }
+    init(suit: String, rank: String) {
+        self.suit = suit
+        self.rank = rank
+    }
 }
--- ClassesMagicFun/House.swift
+++ ClassesMagicFun/House.swift
@@ -9,4 +9,14 @@
 import Foundation
 
 class House: Player {
+    init() {
+        super.init(name: "House")
+    }
+    override func willHit(bet: UInt) -> Bool {
+        if handValue < 17 {
+            return true
+        } else {
+            return false
+        }
+    }
 }
