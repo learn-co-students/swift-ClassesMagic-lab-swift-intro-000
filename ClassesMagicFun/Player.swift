d763f7cf47eb2326fc8d31cf6fba1d7fb9045daf
MianAnswer <miananswer@hotmail.com>
2017-12-21 12:34:25 -0500

    Completed problems

--- ClassesMagicFun/Player.swift
+++ ClassesMagicFun/Player.swift
@@ -7,4 +7,70 @@
 //
 
 class Player {
+    
+    let name : String
+    var wallet : UInt
+    var hand : [Card] = []
+    var handSize : Int {
+        get {
+            return hand.count
+        }
+    }
+    var handValue : UInt {
+        get {
+            var sum : UInt = 0
+            for card in hand {
+                sum += UInt(card.value)
+            }
+            return sum
+        }
+    }
+    var isBlackJack : Bool {
+        get {
+            if(self.handValue == 21) {
+                return true
+            }
+            return false
+        }
+    }
+    var isBusted : Bool {
+        get {
+            if(self.handValue > 21) {
+                return true
+            }
+            return false
+        }
+    }
+    
+    init(name : String) {
+        self.name = name
+        self.wallet = 100
+    }
+    
+    func dealCard(_ card: Card) {
+        hand.append(card)
+    }
+    
+    func canPlaceBet(_ bet: UInt) -> Bool {
+        if(bet < self.wallet) {
+            return true
+        }
+        return false
+    }
+    
+    func willHit(_ bet: UInt) -> Bool {
+        if(bet < self.wallet && self.handSize < 21) {
+            return true
+        }
+        return false
+    }
+    
+    func win(_ amount: UInt) {
+        self.wallet += amount
+    }
+    
+    func lose(_ amount: UInt) {
+        self.wallet -= amount
+    }
+    
 }
