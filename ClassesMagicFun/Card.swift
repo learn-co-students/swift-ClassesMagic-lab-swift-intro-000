88677a6c062a4a69d82bf2f5efaa0864d28083a1
Cynthia Nikolai <cnikolai@earthlink.net>
2018-01-11 18:50:35 -0500

    finished exercise

--- ClassesMagicFun/Card.swift
+++ ClassesMagicFun/Card.swift
@@ -9,7 +9,30 @@
 import Foundation
 
 class Card {
-  
+    let suit:String
+    let rank:String
+    let label:String
+    var value:Int {
+        get {
+            return self.value
+        }
+        set {
+        }
+    }
+    
+    init (rank: String, suit: String) {
+        self.suit = suit
+        self.rank = rank
+        label = "\(self.rank)\(self.suit)"
+        switch rank {
+        case "A":
+            value = 1
+        case "10","J","Q","K":
+            value = 10
+        default:
+            value = Int(rank)!
+        }
+    }
 }
 
 
--- ClassesMagicFun/Dealer.swift
+++ ClassesMagicFun/Dealer.swift
@@ -9,4 +9,61 @@
 import Foundation
 
 class Dealer {
+    var deck:Deck
+    var player:Player
+    var house:House
+    var winner:Player?
+    
+    init() {
+        deck = Deck()
+        player = Player(name: "Sue")
+        house = House()
+        self.playDeck()
+    }
+    
+    func playDeck() {
+        while !deck.isFullyDealt() {
+            playHand()
+        }
+        winner = determineWinner()
+        print("The winner is \(winner!.name)")
+    }
+    
+    func playHand() {
+        if !deck.isFullyDealt() {
+        player.dealCard(card: deck.drawCard()!)
+        }
+          if !deck.isFullyDealt() {
+        player.dealCard(card: deck.drawCard()!)
+        }
+          if !deck.isFullyDealt() {
+        house.dealCard(card: deck.drawCard()!)
+        }
+          if !deck.isFullyDealt() {
+        house.dealCard(card: deck.drawCard()!)
+        }
+        if !house.didWin() {
+            let randomNum = UInt(arc4random_uniform(UInt32(player.wallet)))
+            while player.willHit(randomNum) {
+                  if !deck.isFullyDealt() {
+                    self.player.dealCard(card: deck.drawCard()!)
+                }
+                if self.player.didWin() {
+                    self.player.win(amountWon: randomNum)
+                } else if self.player.didLose() {
+                    self.player.lose(amountLost: randomNum)
+                }
+            }
+        } else {
+            house.win(amountWon: 10)
+        }
+    }
+    
+    func determineWinner() -> Player {
+        if house.wallet > player.wallet {
+            return house
+        } else {
+            return player
+        }
+    }
 }
