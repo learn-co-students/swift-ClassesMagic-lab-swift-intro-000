04c9ee33832976cfc13c1324310b3ec559fc749c
Orvybr0wn1 <orvyb1@gmail.com>
2019-03-30 17:11:17 -0400

    BlackJack Fail

--- ClassesMagicFun/Dealer.swift
+++ ClassesMagicFun/Dealer.swift
@@ -8,5 +8,67 @@
 
 import Foundation
 
-class Dealer {
+class Dealer
+{
+    var deck: Deck
+    var player: Player
+    var house: House
+    var winner: Player?
+    
+    init()
+    {
+        //self.deck
+        self.player = Player (name: "Player")
+        self.house = House()
+        self.deck = Deck()
+    }
+    
+    func deal()
+    {
+        if let firstDraw = deck.drawCard()
+        {
+            player.hand.append(firstDraw)
+        }
+        if let secondDraw = deck.drawCard()
+        {
+            player.hand.append(secondDraw)
+        }
+        
+        if let firstDraw = deck.drawCard()
+        {
+            house.hand.append(firstDraw)
+        }
+        if let secondDraw = deck.drawCard()
+        {
+            house.hand.append(secondDraw)
+        }
+        
+        if (player.handValue == 21)
+        {
+            winner = player
+        }
+        if (house.handValue == 21)
+        {
+            winner = house
+        }
+    }
+    
+    func turn (player: Player)
+    {
+        
+    }
+    
+    func award ()
+    {
+        if winner?.name == "Player"
+        {
+            player.win(winAmount: player.wallet )
+            house.lose(loseAmount: house.wallet)
+        }
+        if winner?.name == "House"
+        {
+            player.win(winAmount: player.wallet )
+            house.lose(loseAmount: house.wallet)
+        }
+    }
 }
